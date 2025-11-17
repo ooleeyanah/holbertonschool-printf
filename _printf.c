b5805a3c09cd8678bb647af2520f1c822d533ac9
Uliana Deshin <uliana.deshin@gmail.com>
2025-11-17 23:01:31 +0000

    skeleton version

--- _printf.c
+++ _printf.c
@@ -6,5 +6,40 @@
  */
 int _printf(const char *format, ...)
 {
-
-  
+va_list args;
+int i = 0, count = 0;
+int (*f)(va_list);
+if (format == NULL)
+return (-1);
+va_start(args, format);
+while (format[i])
+{
+if (format[i] == '%')
+{
+i++;
+if (format[i] == '%')
+{
+count += print_percent();
+}
+else
+{
+f = get_func(format[i]);
+if (f)
+count += f(args);
+else
+{
+write(1, "%", 1);
+write (1, &format[i], 1);
+count += 2;
+}
+}
+}
+else
+{
+count += write(1, &format[i], 1);
+}
+i++;
+}
+va_end(args);
+return (count);
+}
