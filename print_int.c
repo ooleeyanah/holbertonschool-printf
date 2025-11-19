c4ba350311e0ba8b4f4168ed1a94e947a62a0994
Uliana Deshin <uliana.deshin@gmail.com>
2025-11-19 00:49:25 +0000

    print_int

---
+++ print_int.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <limits.h>
+#include <stdarg.h>
+
+/**
+ * print_int - Prints an integer to stdout
+ * @args: va_list containing the integer
+ *
+ * Return: Number of characters printed
+ */
+int print_int(va_list args)
+{
+    int n = va_arg(args, int);
+    int count = 0;
+    if (n == INT_MIN)
+    {
+        count += write(1, "-2147483648", 11);
+        return (count);
+    }
+    if (n < 0)
+    {
+        count += write(1, "-", 1);
+        n = -n;
+    }
+    if (n / 10)
+        count += print_int_digit(n / 10);
+
+    count += print_int_digit(n % 10);
+    return (count);
+}
+
+/**
+ * print_int_digit - Prints a single digit
+ * @d: Digit to print (0–9)
+ * Return: Number of characters printed (always 1)
+ */
+int print_int_digit(int d)
+{
+    char c = d + '0';
+
+    return (write(1, &c, 1));
+}
