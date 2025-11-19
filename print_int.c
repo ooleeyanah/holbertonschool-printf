b748f6f2497084a5848ff38f3df69672d91fb8f0
Uliana Deshin <uliana.deshin@gmail.com>
2025-11-19 01:27:30 +0000

    add print_int_rec

--- print_int.c
+++ print_int.c
@@ -2,34 +2,43 @@
 #include <unistd.h>
 #include <limits.h>
 #include <stdarg.h>
-
 /**
- * print_int - Prints an integer to stdout
- * @args: va_list containing the integer
- *
- * Return: Number of characters printed
+ * print_int_rec - helper to print digits of an integer
+ * @n: number to print
+ * Return: number of chars printed
+ */
+int print_int_rec(int n)
+{
+    int count = 0;
+
+    if (n / 10)
+        count += print_int_rec(n / 10);
+
+    count += print_int_digit(n % 10);
+    return (count);
+}
+/**
+ * print_int - Prints an integer from va_list
+ * @args: va_list
+ * Return: number of characters printed
  */
 int print_int(va_list args)
 {
     int n = va_arg(args, int);
     int count = 0;
+
     if (n == INT_MIN)
-    {
-        count += write(1, "-2147483648", 11);
-        return (count);
-    }
+        return write(1, "-2147483648", 11);
+
     if (n < 0)
     {
         count += write(1, "-", 1);
         n = -n;
     }
-    if (n / 10)
-        count += print_int_digit(n / 10);
 
-    count += print_int_digit(n % 10);
-    return (count);
+    count += print_int_rec(n);
+    return count;
 }
-
 /**
  * print_int_digit - Prints a single digit
  * @d: Digit to print (0–9)
@@ -38,6 +47,5 @@ int print_int(va_list args)
 int print_int_digit(int d)
 {
     char c = d + '0';
-
     return (write(1, &c, 1));
 }
