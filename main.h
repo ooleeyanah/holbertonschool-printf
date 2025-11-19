c96ed0386002353962a132d6df99e8f7a548438e
Uliana Deshin <uliana.deshin@gmail.com>
2025-11-19 01:28:06 +0000

    add print_int_rec

--- main.h
+++ main.h
@@ -8,5 +8,6 @@ int print_string(va_list args);
 int print_int(va_list args);
 int print_percent(void);
 int (*get_func(char c))(va_list);
+int print_int_rec(int n);
 int print_int_digit(int d);
 #endif
