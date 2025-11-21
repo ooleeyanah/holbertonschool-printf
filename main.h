0cdb706d3d9ffe6b3159cf044630baf1554e562a
Uliana Deshin <uliana.deshin@gmail.com>
2025-11-21 23:59:14 +0000

    add print_binary

--- main.h
+++ main.h
@@ -7,6 +7,7 @@ int print_char(va_list args);
 int print_string(va_list args);
 int print_int(va_list args);
 int print_percent(void);
+int print_binary(va_list args);
 int (*get_func(char c))(va_list);
 int print_int_rec(int n);
 int print_int_digit(int d);
