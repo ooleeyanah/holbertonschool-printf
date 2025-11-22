8f2b6113c51a1f8f8ba867cdfb889729c7364ef5
Uliana Deshin <uliana.deshin@gmail.com>
2025-11-22 00:07:48 +0000

    print unsigned, octal, hex lower and hex upper

--- get_func.c
+++ get_func.c
@@ -12,6 +12,14 @@ if (c == 's')
 return (print_string);
  if (c == 'd' || c == 'i')
    return (print_int);
+ if (c == 'u')
+   return (print_unsigned)
+     if (c == 'o')
+       return (print_octal);
+ if (c == 'x')
+   return (print_hex_lower);
+ if (c == 'X')
+   return (print_hex_upper);
  if (c == 'b')
    return (print_binary);
  return (NULL);
