f9e025b67b01cc7c10436c6c70f7bc6c531b31c7
Uliana Deshin <uliana.deshin@gmail.com>
2025-11-22 01:05:11 +0000

    print to handle

--- get_func.c
+++ get_func.c
@@ -4,23 +4,23 @@
  * @c: value
  * Return: function or null
  */
-int (*get_func(char c))(va_list)
+int (*get_func(char c))(va_list, char *, int *, int *)
 {
-if (c == 'c')
-return (print_char);
-if (c == 's')
-return (print_string);
- if (c == 'd' || c == 'i')
-   return (print_int);
- if (c == 'u')
-   return (print_unsigned);
-     if (c == 'o')
-       return (print_octal);
- if (c == 'x')
-   return (print_hex_lower);
- if (c == 'X')
-   return (print_hex_upper);
- if (c == 'b')
-   return (print_binary);
- return (NULL);
+    if (c == 'c')
+        return (handle_char);
+    if (c == 's')
+        return (handle_string);
+    if (c == 'd' || c == 'i')
+        return (handle_int);
+    if (c == 'u')
+        return (handle_uint);
+    if (c == 'o')
+        return (handle_octal);
+    if (c == 'x')
+        return (handle_hex_lower);
+    if (c == 'X')
+        return (handle_hex_upper);
+    if (c == 'b')
+        return (handle_binary);
+    return (NULL);
 }
